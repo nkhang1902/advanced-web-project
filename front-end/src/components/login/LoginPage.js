1e70731eb04b5d2c8e2a4485773d36cfe855efa3
phattrieu <86005213+phatng45@users.noreply.github.com>
2023-11-11 17:47:24 +0700

    update login page

--- front-end/src/components/login/LoginPage.js
+++ front-end/src/components/login/LoginPage.js
@@ -1,79 +1,87 @@
-import {Button, Checkbox, Form, Input, Layout} from "antd";
-import {LockOutlined, UserOutlined} from "@ant-design/icons";
+import {Button, Checkbox, Col, Divider, Flex, FloatButton, Form, Input, Layout, Row, Space} from "antd";
 import React from "react";
 import {Content} from "antd/es/layout/layout";
 import Sider from "antd/es/layout/Sider";
 import './LoginPage.css'
 import {Footer} from "antd/lib/layout/layout";
+import {ArrowRightOutlined} from "@ant-design/icons";
 
 export function LoginPage() {
 
     const loginForm = <Form
         name="login"
-        // onFinish={onFinish}
         initialValues={{remember: true}}>
         <Form.Item
             name="username"
-            rules={[
-                {
-                    required: true,
-                    message: 'Please enter your username!',
-                },
-            ]}>
+            rules={[{
+                required: true, message: 'Please enter your username!',
+            },]}>
             <Input
-                prefix={<UserOutlined className="site-form-item-icon"/>}
                 placeholder="Username"
+                size={"large"}
             />
         </Form.Item>
         <Form.Item
             name="password"
-            rules={[
-                {
-                    required: true,
-                    message: 'Please enter your password!',
-                },
-            ]}>
+            rules={[{
+                required: true, message: 'Please enter your password!',
+            },]}>
             <Input
-                prefix={<LockOutlined className="site-form-item-icon"/>}
                 type="password"
                 placeholder="Password"
+                size={"large"}
             />
         </Form.Item>
         <Form.Item>
             <Form.Item name="remember" valuePropName="checked" noStyle>
                 <Checkbox>Remember me</Checkbox>
             </Form.Item>
-            Forgot password
-            {/*<Link to="/forgot-password">Forgot password</Link>*/}
         </Form.Item>
-
-        <Form.Item>
-            <Button type="primary" htmlType="submit" className="login-form-button">
-                Log in
-            </Button>
-            register now!
-            {/*Or <Link to="/register">register now!</Link>*/}
-        </Form.Item>
+        <center>
+            <Form.Item>
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                    <ArrowRightOutlined style={{fontSize: 20}}/>
+                </Button>
+            </Form.Item>
+        </center>
     </Form>;
 
 
-    return (
-        <Layout style={{minHeight: '100vh'}}>
-            <Layout>
-                <Content style={{background: "white"}}>
-                    <img
-                        className={"content-image"}
-                        src="https://img.freepik.com/free-vector/forming-team-leadership-concept-illustration_114360-13973.jpg?w=1380&t=st=1699185991~exp=1699186591~hmac=db5867fc0d5f1c177db04cedad0350055d285bcf7df500586a960e16c2c64390"
-                        alt="Login image"/>
-                </Content>
-                <Footer>
-                    <b>HCMUS - Advanced Web Programming Course</b>
-                    <br/> Members:
-                </Footer></Layout>
-            <Sider width={'33%'} class="login-form-container">
-                <h1 className="welcome-text">Welcome back!</h1>
-                {loginForm}
-            </Sider>
+    return (<Layout style={{minHeight: '100vh'}}>
+        <Layout style={{maxWidth: '66%',  minHeight:'100%'}}>
+            <Content style={{position:'relative'}}>
+                <img
+                    className={'login-image'}
+                    src="https://i.imgur.com/8rAYT95.jpeg"
+                    alt="Login image"/>
+                <p className="welcome-text-2">Lorem Ipsum</p>
+            </Content>
         </Layout>
-    );
+        <Sider width={'33%'} class="login-form-container">
+            <Flex vertical justify={"space-between"} style={{minHeight: '100%', padding: '10% 10% 0'}}>
+                <div>
+                    <center><h1 className="welcome-text">Login</h1></center>
+                    {loginForm}
+                </div>
+                <center>
+                    <Button type={"link"} className={"secondary-button"}><b>COULD NOT
+                        LOGIN?</b></Button>
+                    <br/>
+                    <Button type={"link"} className={"secondary-button"}><b>CREATE A NEW
+                        ACCOUNT</b></Button>
+                    <p style={{
+                        color: "silver",
+                        textTransform: "uppercase",
+                        fontFamily: "Poppins",
+                        fontSize: 12
+                    }}>
+                        HCMUS - Advanced Web Programming Course
+                        <br/>This project is for educational purpose only. All icons, logos and contents used are
+                        the
+                        sole properties of their respective owners.
+                    </p>
+                </center>
+            </Flex>
+        </Sider>
+    </Layout>);
 }
