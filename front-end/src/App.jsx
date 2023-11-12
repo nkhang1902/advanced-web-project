b1bc2b9215bc9b51cd9906ebc81c03016b20dbe4
nkhang1902 <nkhang1902@gmail.com>
2023-11-12 11:12:43 +0700

    homepage init + routerbrowser

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,18 @@
 import {AppLayout} from "./components/app-layout/AppLayout";
+import HomePage from "./components/homepage/homepage";
 import {LoginPage} from "./components/login/LoginPage";
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import SignUpPage from "./components/signup/SignUpPage";
 
 function App() {
     return (
-        <Router>
-        <AppLayout>
-            <Routes>
-                <Route path="/login" element={<LoginPage />}/>
-            </Routes>
-        </AppLayout>
-        </Router>
+            <AppLayout>
+                <Routes>
+                    <Route path="/login" element={<LoginPage />}/>
+                    <Route path="/signup" element={<SignUpPage/>}/>
+                    <Route path="/" element={<HomePage/>}/>
+                </Routes>
+            </AppLayout>
         // <LoginPage/>
     );}
 
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,11 +9,15 @@ import {ConfigProvider} from "antd";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+        <BrowserRouter>
         <ConfigProvider theme={{
             token: {
                 colorPrimary: "royalblue"
             }
-        }}><App/></ConfigProvider>
+        }}>
+            <App/>
+        </ConfigProvider>
+        </BrowserRouter>
     </React.StrictMode>
 );
 
