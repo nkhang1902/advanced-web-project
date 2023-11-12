b694095a67cf5deaf76f6cf2d8658f8d1c6c7083
nkhang1902 <nkhang1902@gmail.com>
2023-11-12 13:32:09 +0700

    Update SignUpPage.jsx

--- front-end/src/components/signup/SignUpPage.jsx
+++ front-end/src/components/signup/SignUpPage.jsx
@@ -1,38 +1,107 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {Button, Checkbox, Col, Divider, Flex, FloatButton, Form, Input, Layout, Row, Space} from "antd";
+import {Content} from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
+import "../login/LoginPage.css"
+import {Footer} from "antd/lib/layout/layout";
+import {ArrowRightOutlined} from "@ant-design/icons";
 
 function SignUpPage() {
-    return (
-        <div class="w-1/3 mt-8 mx-auto">
-            <h1 class="mb-4 font-extrabold text-2xl">Register</h1>
-            <div class="grid grid-cols-1 md:grid-cols-1 gap-8">
-                <form>
-                    <div>
-                        <label class="block font-semibold" for="name">Name</label>
-                        <input class="w-full shadow-inner bg-gray-100 rounded-lg placeholder-black text-md p-4 border-none block mt-1 w-full" id="name" type="text" name="name" required="required" autofocus="autofocus"/>
-                    </div>
+    return (<Layout style={{minHeight: '100vh'}}>
+        <Layout style={{maxWidth: '66%',  minHeight:'100%'}}>
+            <Content style={{position:'relative'}}>
+                <img
+                    className={'login-image'}
+                    src="https://i.imgur.com/8rAYT95.jpeg"
+                    alt="Login image"/>
+                <p className="welcome-text-2">Lorem Ipsum</p>
+            </Content>
+        </Layout>
+        <Sider width={'33%'} class="login-form-container">
+            <Flex vertical justify={"space-between"} style={{minHeight: '100%', padding: '10% 10% 0'}}>
+                <div>
+                    <center><h1 className="welcome-text">Register</h1></center>
+                    <Form
+                        name="login"
+                        initialValues={{remember: true}}>
+                        <Form.Item
+                            name="username"
+                            rules={[{
+                                required: true, message: 'Please enter your username!',
+                            },]}>
+                            <Input
+                                placeholder="Username"
+                                size={"large"}
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            name="email"
+                            rules={[{
+                                required: true, message: 'Please enter your email!',
+                            },]}>
+                            <Input
+                                type="email"
+                                placeholder="Email"
+                                size={"large"}
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[{
+                                required: true, message: 'Please enter your password!',
+                            },]}>
+                            <Input
+                                type="password"
+                                placeholder="Password"
+                                size={"large"}
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            name="repassword"
+                            rules={[{
+                                required: true, message: 'Please re-enter your password!',
+                            },]}>
+                            <Input
+                                type="repassword"
+                                placeholder="Confirm Password"
+                                size={"large"}
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Form.Item name="remember" valuePropName="checked" noStyle>
+                                <Checkbox>Remember me</Checkbox>
+                            </Form.Item>
+                        </Form.Item>
+                        <center>
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" className="login-form-button bg-blue-400">
+                                    <ArrowRightOutlined style={{fontSize: 20}}/>
+                                </Button>
+                            </Form.Item>
+                            <Link to="/login" class="font-semibold">
+                                Already registered?
+                            </Link>
+                        </center>
 
-                    <div class="mt-4">
-                        <label class="block font-semibold" for="email">Email</label>
-                        <input class="w-full shadow-inner bg-gray-100 rounded-lg placeholder-black text-md p-4 border-none block mt-1 w-full" id="email" type="email" name="email" required="required"/>
-                    </div>
-
-                    <div class="mt-4">
-                        <label class="block font-semibold" for="password">Password</label>
-                        <input class="w-full shadow-inner bg-gray-100 rounded-lg placeholder-black text-md p-4 border-none block mt-1 w-full" id="password" type="password" name="password" required="required" autocomplete="new-password"/>
-                    </div>
-
-                    <div class="flex items-center justify-between mt-8">
-                        <button type="submit" class="flex items-center justify-center px-2 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">Register</button>
-                        <Link to="/login" class="font-semibold">
-                            Already registered?
-                        </Link>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
-
+                    </Form>
+                </div>
+                <center>
+                    <p style={{
+                        color: "silver",
+                        textTransform: "uppercase",
+                        fontFamily: "Poppins",
+                        fontSize: 12
+                    }}>
+                        HCMUS - Advanced Web Programming Course
+                        <br/>This project is for educational purpose only. All icons, logos and contents used are
+                        the
+                        sole properties of their respective owners.
+                    </p>
+                </center>
+            </Flex>
+        </Sider>
+    </Layout>);
 }
 
 export default SignUpPage;
