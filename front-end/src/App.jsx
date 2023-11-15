6ea7912efc4a79bb12a6eee13e0b3ec56860feea
phattrieu <86005213+phatng45@users.noreply.github.com>
2023-11-15 20:41:53 +0700

    Update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes, useNavigate } from "react-router-dom";
-import LoginPage from "./components/Login/LoginPage";
-import SignUpPage from "./components/SignUp/SignUpPage";
-import HomePage from "./components/HomePage/HomePage";
+import LoginPage from "./components/login/LoginPage";
+import SignUpPage from "./components/signup/SignUpPage";
+import HomePage from "./components/homepage/homepage";
 import UpdateProfile from "./components/UpdateProfile/UpdateProfile";
 
 // const isAuthenticated = false;
@@ -14,10 +14,10 @@ function App() {
   // }
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/" element={<HomePage />} />
-      <Route path="/update-profile" element={<UpdateProfile />} />
+      <Route path="/login" element={<LoginPage/>} />
+      <Route path="/signup" element={<SignUpPage/>} />
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/update-profile" element={<UpdateProfile/>} />
     </Routes>
   );
 }
