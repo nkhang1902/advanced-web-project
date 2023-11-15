ec4b31561378523092751fc7696f3be1a1abfb0a
phattrieu <86005213+phatng45@users.noreply.github.com>
2023-11-15 20:44:41 +0700

    Update SignUpPage.jsx

--- front-end/src/components/signup/SignUpPage.jsx
+++ front-end/src/components/signup/SignUpPage.jsx
@@ -5,7 +5,7 @@ import { Content } from "antd/es/layout/layout";
 import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../Login/LoginPage.css";
+import "../login/LoginPage.css";
 
 function SignUpPage() {
   const navigate = useNavigate();
